5d108fae6669cca621576d61fd61085bdfab3185
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-05-13 18:52:11 +0500

    Learn about Form Processing in Express

--- Using-Express/meadowlark.js
+++ Using-Express/meadowlark.js
@@ -38,6 +38,13 @@ app.get('/newsletter', function(req, res){
     res.render('newsletter', {csrf: 'CSRF token goes here'});
 });
 
+app.post('/process', function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF Token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');
+});
 
 // routes go here
 app.get("/", function (req, res) {
