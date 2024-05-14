b23ed1defa9841729d73081de6456918416ac785
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-05-14 22:55:17 +0500

    Learn about how to handle image or other files in forms, how to access in express

--- Using-Express/meadowlark.js
+++ Using-Express/meadowlark.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var fortune = require("./lib/fortune.js");
+var formidable = require("formidable")
 
 var app = express();
 
@@ -58,6 +59,27 @@ app.post('/process', function(req, res){
     {
         res.redirect(303, '/thank-you');
     }
+});
+
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year : now.getFullYear(),
+        month : now.getMonth(),
+    });
+});
+
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if (err) return res.redirect(303, '/error');
+        console.log('Received fields:');
+        console.log(fields);
+        console.log('Received Files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
 });
 
 // routes go here
