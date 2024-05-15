0d057f8ea66c90747322f08e09c51d775156fafb
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-05-15 13:08:55 +0500

    learn about how to setup flash

--- Using-Express/meadowlark.js
+++ Using-Express/meadowlark.js
@@ -66,6 +66,41 @@ app.post('/process', function(req, res){
     res.redirect(303, '/thank-you');
 });
 
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    // input validation
+    if (!email.match(VALID_EMAIL_REGEX)){
+        if (req.xhr) return res.json({error: 'Ivalid name email address.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message : 'The email address you entered was not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+
+    new NewsletterSignup({ name: name, email: email}).save(function(err){
+        if (err){
+            if (req.xhr) return res.json({error: 'Database Error'});
+            req.session.flash = {
+                type : 'danger',
+                intro : 'Database error!',
+                message : 'There was a database error; please try again later.',
+            };
+            return redirect(303, '/newsletter/archive');
+        }
+        if (req.xhr) return res.json({success: true});
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank You',
+            message: 'You have been signed up for the newsletter.'
+        };
+        res.redirect(303, '/newsletter/archive');
+    });
+});
+
 app.get('/thank-you', function(req, res){
     res.render('thank-you');
 });
