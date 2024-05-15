c514a369dc2cb5eb9fadd5c5a6488f70b7c8e492
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-05-15 12:46:19 +0500

    Set the flash middleware and fix some bugs

--- Using-Express/meadowlark.js
+++ Using-Express/meadowlark.js
@@ -52,8 +52,6 @@ app.use(function (req, res, next) {
 });
 
 
-
-
 // Chapter 8 Form Processing
 app.get('/newsletter', function(req, res){
     // we will learn about CSRF later... for now, we just provide a dummy value
